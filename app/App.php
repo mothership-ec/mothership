2585450751a0d301dff2a225dd745ab9fd735ebe
thomasjthomasj <thomasjmarchant@hotmail.com>
2016-03-18 15:47:45 +0000

    install mailing module

--- app/App.php
+++ app/App.php
@@ -18,6 +18,7 @@ class App extends \Message\Cog\Application\Loader
 			'Message\\Mothership\\Voucher',
 			'Message\\Mothership\\Discount',
 			'Message\\Mothership\\Report',
+			'Message\\Mothership\\Mailing',
 			'Mothership\\Site',
 		];
 	}
